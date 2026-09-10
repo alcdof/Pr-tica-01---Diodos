29a769fd846760935d45d0f8af64a9a2764a4846
André Caillot <andreluizfilho@alunos.utfpr.edu.br>
2026-09-10 12:26:48 -0300

    plot_Id versão 1

---
+++ plot_Id.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+tensao_1N400x = [-3.213, -1.1268, -0.0310, 0.1924, 0.3726, 0.4589, 0.5292, 0.5943, 0.6233, 0.6424, 0.6525, 0.6565, 0.7134, 0.7362, 0.7614, 0.7814, 0.7895]
+corrente_1N400x = [-0.064, -0.015, -0.022, -0.029, -0.019, 0.043, 0.243, 1.019, 1.835, 2.680, 3.516, 3.866, 13.415, 22.907, 43.269, 72.840, 93.390]
+tensao_1N4148 = [-3.0642, -1.1129, -0.0067, 0.1567, 0.3734, 0.4280, 0.5587, 0.5954, 0.6366, 0.6531, 0.6785, 0.6869, 0.7636, 0.8009, 0.8490, 0.8939, 0.9128]
+corrente_1N4148 = [-0.003, -0.006, -0.012, -0.072, -0.002, 0.043, 0.322, 0.691, 1.667, 2.116, 2.836, 3.904, 13.291, 22.636, 42.585, 72.080, 91.450]
+
+plt.xlabel(r'$V_D$ (V)')
+plt.ylabel(r'$I_d$ (mA)')
+plt.title('Gráfico da corrente em um diodo em função da tensão aplicada sobre ele')
+plt.grid(True)
+
+plt.plot(tensao_1N4148, corrente_1N4148, label='Diodo 1N4148')
+plt.plot(tensao_1N400x, corrente_1N400x, label='Diodo 1N400x')
+
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+
+plt.tight_layout()
+plt.show()
